e9a18916a0ecac4c6b5b45aea4ee3ad1506b0f30
Tsion Wubshet <t687959@gmail.com>
2026-04-25 12:18:14 +0300

    authentication

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
+import authRoutes from "./routes/authRoutes";
 
 dotenv.config();
 connectDB();
@@ -8,6 +9,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("KindHands API running");
